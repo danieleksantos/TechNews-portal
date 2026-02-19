f47cc86ff00ebdd2ef11b37c844290149bdd8016
Daniele Karina dos Santos <danimistron@gmail.com>
2026-02-19 14:23:57 -0300

    fix: allow image domain

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.unsplash.com', // O asterisco duplo aceita qualquer subdomínio do Unsplash
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com', // Adicionando explicitamente o subdomínio do print
+        port: '',
+        pathname: '/**',
       },
     ],
   },
 }
+
+module.exports = nextConfig;
